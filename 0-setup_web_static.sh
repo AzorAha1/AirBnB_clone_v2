be4712188df2aab5321d99c9e691921861d42f37
AzorAha1 <wizfaiz@icloud.com>
2024-01-11 21:49:35 +0100

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,28 +1,22 @@
 #!/usr/bin/env bash
 # sets up web servers for the deployment of web_static
 
-source=/data/web_static/releases/test/
-symlink=/data/web_static/current
-configcode="    location /hbnb_static {\n\talias /data/web_static/current/;\n\tindex index.html;\n\t}"
-configfile=/etc/nginx/sites-enabled/default
-htmlcontent="
-<html>
-    <head>
-    </head>
-    <body>
-        Holberton School
-    </body>
-</html>
-"
 sudo apt-get -y update
 sudo apt-get -y install nginx
+
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-echo "$htmlcontent" | sudo tee -a "$source/index.html"
-sudo ln -sf $source $symlink
+sudo echo '<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>' | sudo tee "/data/web_static/releases/test/index.html"
+sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "/server {/a $configcode" "$configfile"
+sudo sed -i "listen 80 {/a ""location /hbnb_static { alias /data/web_static/current/;}" /etc/nginx/sites-enabled/default
 sudo service nginx restart
