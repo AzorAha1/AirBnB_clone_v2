92ef4795bbb4aa6ffe49639fa3cd50897daec5ac
AzorAha1 <wizfaiz@icloud.com>
2024-01-11 23:27:02 +0100

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,14 +3,16 @@
 from fabric.operations import local
 import os
 from datetime import datetime
+
+
 def do_pack():
     time = datetime.now().strftime("%Y%m%d%H%M%S")
     archive_name = f"web_static_{time}.tgz"
     source = "web_static"
     os.makedirs(destination, exist_ok=True)
     destination = "versions"
-    local(f'tar -czvf {destination}/{archive_name} {source}')
-   
-    return os.path.join(destination, archive_name)
-
-do_pack()
+    run = local(f'tar -czvf {destination}/{archive_name} {source}')
+    if run.failed:
+        return None
+    else:
+        return os.path.join(destination, archive_name)
