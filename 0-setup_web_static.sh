f00bcc94f26a11503215811eeaa83b7a2bff6b9b
AzorAha1 <wizfaiz@icloud.com>
2024-01-11 21:03:30 +0100

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,6 +5,13 @@ source=/data/web_static/releases/test/
 symlink=/data/web_static/current
 configcode="    location /hbnb_static {\n\talias /data/web_static/current/;\n\tindex index.html;\n\t}"
 configfile=/etc/nginx/sites-enabled/default
+htmlcontent="
+<html>
+    <body>
+        Holberton School
+    </body>
+</html>
+"
 if ! command -v nginx &> /dev/null; then
     sudo apt-get update
     sudo apt-get install -y nginx
@@ -14,7 +21,7 @@ sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-echo "snippet content" | sudo tee -a "$source/index.html"
+echo "$htmlcontent" | sudo tee -a "$source/index.html"
 if [ -e "$symlink" ]; then
     sudo rm "$symlink"
 fi
