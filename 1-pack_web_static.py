a7d47505aaeb3b1f45cb9e805dcf1ed8be394bed
AzorAha1 <wizfaiz@icloud.com>
2024-01-11 23:16:00 +0100

    update

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""fabric script that generates an archive"""
+from fabric.operations import local
+import os
+from datetime import datetime
+def do_pack():
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = f"web_static_{time}.tgz"
+    source = "web_static"
+    os.makedirs(destination, exist_ok=True)
+    destination = "versions"
+    run = local(f'tar -czvf {destination}/{archive_name} {source}')
+    if run.failed:
+        return None
+    else:
+        return os.path.join(destination, archive_name)
