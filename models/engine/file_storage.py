3937d1008286d395b04874581e17884e11e31b18
AzorAha1 <wizfaiz@icloud.com>
2024-01-28 21:53:59 +0100

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,7 +68,6 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-    
+
     def close(self):
         self.reload()
-   
