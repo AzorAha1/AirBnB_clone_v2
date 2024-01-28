64f2062afcc5ee452529b7745f0cd819aab52d0c
AzorAha1 <wizfaiz@icloud.com>
2024-01-28 23:36:29 +0100

    update

--- models/state.py
+++ models/state.py
@@ -11,20 +11,17 @@ from models import hbnb_storage
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    
     name = Column(String(128), nullable=False)
-
     cities = relationship(
-            'City', backref='State', cascade='all, delete-orphan',
-            single_parent=True
-            )
-
+            'City', backref='state', cascade='all, delete-orphan')
 
     @property
     def cities(self):
         """Returns City instances"""
         from models import storage
         from models.city import City
-        all_cities = storage.all(City)
-        return [city for city in all_cities.values()
-                if city.state_id == self.id]
+        listofcities = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                listofcities.append(city)
+        return listofcities
