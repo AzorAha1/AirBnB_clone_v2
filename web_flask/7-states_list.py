3b51f28231e56677496ef88970fe596a23419bcf
AzorAha1 <wizfaiz@icloud.com>
2024-01-29 00:51:40 +0100

    update

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""this is my comment"""
+
+
+from flask import Flask
+from flask import escape
+from flask import render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown():
+    """remove the current sqlalchemy sesssion"""
+    storage.close()
+
+
+@app.route('/states_list')
+def states(state):
+    state = storage.all()
+    return render_template('7-states_list.html')
